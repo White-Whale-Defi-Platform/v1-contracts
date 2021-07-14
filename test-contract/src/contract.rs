c2ac56aa1d8d53be8ee0eec29c414c32a41696a5
Lars Lubkoll <lars.lubkoll@posteo.de>
2021-07-14 22:01:13 +0200

    Send both, burn/mint and terraswap, messages at once

--- test-contract/src/contract.rs
+++ test-contract/src/contract.rs
@@ -1,14 +1,14 @@
 use cosmwasm_std::{
     to_binary, Api, Binary, Coin, CosmosMsg, Env, Extern, HandleResponse, HumanAddr, InitResponse, Querier, StdError,
-    StdResult, Storage, WasmMsg
+    StdResult, Storage, WasmMsg, Uint128
 };
-use terra_cosmwasm::{create_swap_msg, TerraRoute, TerraMsgWrapper};
+use terra_cosmwasm::{create_swap_msg, TerraMsgWrapper};
 
 use crate::pair::{HandleMsg as PairMsg};
 use crate::asset::{Asset, AssetInfo};
 
-use crate::msg::{HandleMsg, InitMsg, QueryMsg, WhiteWhaleMsgWrapper, WhiteWhaleMsg};
-use crate::state::{config, State, BURN_MINT_CONTRACT, LUNA_UST_PAIR};
+use crate::msg::{HandleMsg, InitMsg, QueryMsg};
+use crate::state::{config, State, LUNA_UST_PAIR};
 
 pub fn init<S: Storage, A: Api, Q: Querier>(
     deps: &mut Extern<S, A, Q>,
@@ -29,65 +29,45 @@ pub fn handle<S: Storage, A: Api, Q: Querier>(
     env: Env,
     msg: HandleMsg,
 ) -> StdResult<HandleResponse<TerraMsgWrapper>> {
-// ) -> StdResult<HandleResponse<CosmosMsg>> {
     match msg {
         HandleMsg::Trade { amount } => try_trade(deps, env, amount)
     }
 }
+
 pub fn try_trade<S: Storage, A: Api, Q: Querier>(
     _deps: &mut Extern<S, A, Q>,
     env: Env,
     amount: Coin,
 ) -> StdResult<HandleResponse<TerraMsgWrapper>> {
-// ) -> StdResult<HandleResponse<CosmosMsg>> {
-
-    // let offer = Asset{
-    //     info: AssetInfo::NativeToken{ denom: String::from("uusd") },
-    //     amount: amount.amount
-    // };
-    // let terraswap_msg = PairMsg::Swap{
-    //     offer_asset: offer,
-    //     belief_price: None,
-    //     max_spread: None,
-    //     to: None,
-    // };
-    // let csm_terraswap_msg = CosmosMsg::Wasm(WasmMsg::Execute {
-    //     contract_addr: HumanAddr::from(LUNA_UST_PAIR.clone()),
-    //     send: vec![amount.clone()],
-    //     msg: to_binary(&terraswap_msg)?,
-    // });
 
     let ask_denom = "uluna".to_string();
+    let offer = Asset{
+        info: AssetInfo::NativeToken{ denom: ask_denom.clone() },
+        amount: Uint128(5000)
+    };
+    let terraswap_msg = PairMsg::Swap{
+        offer_asset: offer,
+        belief_price: None,
+        max_spread: None,
+        to: None,
+    };
+    let csm_terraswap_msg = CosmosMsg::Wasm(WasmMsg::Execute {
+        contract_addr: HumanAddr::from(LUNA_UST_PAIR.clone()),
+        send: vec![Coin{ denom: ask_denom.clone(), amount: Uint128(5000)}],
+        msg: to_binary(&terraswap_msg)?,
+    });
+
     let swap_msg = create_swap_msg(
         env.contract.address,
         amount,
         ask_denom,
     );
-    // let swap_msg = Swap {
-    //     route: TerraRoute::Market,
-    //     msg_data: WhiteWhaleMsg::Swap {
-    //         trader,
-    //         offer_coin,
-    //         ask_denom,
-    //     },
-    // };
-    // let csm_swap_msg = CosmosMsg::Wasm(WasmMsg::Execute {
-    //     contract_addr: HumanAddr::from(BURN_MINT_CONTRACT.clone()),
-    //     send: vec![],
-    //     msg: to_binary(&swap_msg)?,
-    // });
- 
+
     Ok(HandleResponse {
-        messages: vec![swap_msg],
+        messages: vec![swap_msg, csm_terraswap_msg],
         log: vec![],
         data: None,
     })
- 
-    // Ok(HandleResponse {
-    //     messages: vec![csm_terraswap_msg],
-    //     log: vec![],
-    //     data: None,
-    // })
 }
 
 pub fn query<S: Storage, A: Api, Q: Querier>(
