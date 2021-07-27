a56c29979ccc7327b3b5413f0c25359839b0d09c
Lars Lubkoll <lars.lubkoll@posteo.de>
2021-07-27 15:48:30 +0200

    Try to recover from connection errors

--- test-contract/scripts/tequila-0004/loop.py
+++ test-contract/scripts/tequila-0004/loop.py
@@ -1,10 +1,16 @@
+import aiohttp
 import asyncio
 from datetime import timedelta
 
+import terra_sdk
 
-async def loop(op, sleep_time: timedelta):
+
+async def loop(op, sleep_time: timedelta, recover_time: timedelta = timedelta(seconds=10)):
     while True:
-        op()
+        try:
+            op()
+        except (aiohttp.client_exceptions.ClientConnectorError, terra_sdk.exceptions.LCDResponseError):
+            await asyncio.sleep(recover_time.total_seconds())
         await asyncio.sleep(sleep_time.total_seconds())
 
 
