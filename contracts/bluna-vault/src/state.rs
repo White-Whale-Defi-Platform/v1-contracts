5311f0b8828f76a366195aed58ca54e96c1bf899
abstruse-goose <23662082+abstruse-goose@users.noreply.github.com>
2021-08-11 19:26:47 +0000

    Migrate bluna-vault to col-5

--- contracts/bluna-vault/src/state.rs
+++ contracts/bluna-vault/src/state.rs
@@ -1,14 +1,12 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{CanonicalAddr, StdResult, Storage};
-use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
-// use terraswap::asset::PairInfoRaw;
-use crate::pool_info::PoolInfoRaw as PairInfoRaw;
+use cosmwasm_std::CanonicalAddr;
+use cw_storage_plus::Item;
 
-pub static CONFIG_KEY: &[u8] = b"config";
-static KEY_PAIR_INFO: &[u8] = b"asset_info";
-pub static BURN_MINT_CONTRACT: &str = "terra1z3sf42ywpuhxdh78rr5vyqxpaxa0dx657x5trs"; // seignorage contract on tequila-0004
+
+use crate::pool_info::PoolInfoRaw;
+
 pub static LUNA_DENOM: &str = "uluna";
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -20,19 +18,5 @@ pub struct State {
     pub bluna_address: CanonicalAddr,
 }
 
-pub fn config<S: Storage>(storage: &mut S) -> Singleton<S, State> {
-    singleton(storage, CONFIG_KEY)
-}
-
-pub fn config_read<S: Storage>(storage: &S) -> ReadonlySingleton<S, State> {
-    singleton_read(storage, CONFIG_KEY)
-}
-
-
-pub fn store_pool_info<S: Storage>(storage: &mut S, data: &PairInfoRaw) -> StdResult<()> {
-    Singleton::new(storage, KEY_PAIR_INFO).save(data)
-}
-
-pub fn read_pool_info<S: Storage>(storage: &S) -> StdResult<PairInfoRaw> {
-    ReadonlySingleton::new(storage, KEY_PAIR_INFO).load()
-}
+pub const STATE: Item<State> = Item::new("\u{0}{5}state");
+pub const POOL_INFO: Item<PoolInfoRaw> = Item::new("\u{0}{4}pool");
