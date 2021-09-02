6fcee75a68211ebd60cfb318796a16782cdbf83f
abstruse-goose <23662082+abstruse-goose@users.noreply.github.com>
2021-09-02 17:44:36 +0000

    Add buy-and-burn vault

---
+++ contracts/buy-and-burn/src/state.rs
@@ -0,0 +1,21 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use cosmwasm_std::{ Storage};
+use cosmwasm_storage::{singleton, singleton_read, ReadonlySingleton, Singleton};
+
+pub static CONFIG_KEY: &[u8] = b"config";
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct State {
+    pub owner_addr: String,
+    pub whale_token_addr: String,
+}
+
+pub fn config(storage: &mut dyn Storage) -> Singleton<State> {
+    singleton(storage, CONFIG_KEY)
+}
+
+pub fn config_read(storage: &dyn Storage) -> ReadonlySingleton<State> {
+    singleton_read(storage, CONFIG_KEY)
+}
