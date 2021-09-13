8d92e6a84748da36c5f38f7ff833e0f48c8762e8
0xFable <0xfable@protonmail.com>
2021-09-13 08:46:30 +0100

    feat(tests): Add tests for spending and config

--- contracts/war-chest/src/testing/tests.rs
+++ contracts/war-chest/src/testing/tests.rs
@@ -1,13 +1,39 @@
 
 use crate::msg::{ConfigResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::contract::{execute, instantiate, query};
+use crate::error::{ContractError};
 use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info};
-use cosmwasm_std::{from_binary, Uint128};
+use cosmwasm_std::{from_binary, to_binary, Api, CosmosMsg, MessageInfo, SubMsg, Uint128, StdError, WasmMsg};
+use cw20::Cw20ExecuteMsg;
 
 
 
 #[test]
 fn proper_initialization() {
+    let mut deps = mock_dependencies(&[]);
+
+    let msg = InstantiateMsg {
+        gov_contract: "gov".to_string(),
+        whale_token: "whale".to_string(),
+        spend_limit: Uint128::from(1_000_000u128),
+    };
+
+    let info = mock_info("addr0000", &[]);
+
+    // we can just call .unwrap() to assert this was a success
+    let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+
+    // it worked, let's query the state
+    let config: ConfigResponse =
+        from_binary(&query(deps.as_ref(), mock_env(), QueryMsg::GetConfig {}).unwrap()).unwrap();
+    assert_eq!("gov", config.gov_contract.as_str());
+    assert_eq!("whale", config.whale_token.as_str());
+    assert_eq!(Uint128::from(1_000_000u128), config.spend_limit);
+}
+
+
+#[test]
+fn update_config() {
     let mut deps = mock_dependencies(&[]);
 
     let msg = InstantiateMsg {
@@ -27,4 +53,98 @@ fn proper_initialization() {
     assert_eq!("gov", config.gov_contract.as_str());
     assert_eq!("whale", config.whale_token.as_str());
     assert_eq!(Uint128::from(1000000u128), config.spend_limit);
+
+    let msg = ExecuteMsg::UpdateConfig {
+        spend_limit: Some(Uint128::from(500000u128)),
+    };
+    let info = mock_info("addr0000", &[]);
+    match execute(deps.as_mut(), mock_env(), info, msg.clone()) {
+        Ok(_) => panic!("Must return error"),
+        Err(ContractError::Unauthorized{ .. }) => (),
+        Err(_) => panic!("Unknown error"),
+    }
+
+    let info = mock_info("gov", &[]);
+    let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+    let config: ConfigResponse =
+        from_binary(&query(deps.as_ref(), mock_env(), QueryMsg::GetConfig {}).unwrap()).unwrap();
+    assert_eq!(
+        config,
+        ConfigResponse {
+            gov_contract: "gov".to_string(),
+            whale_token: "whale".to_string(),
+            spend_limit: Uint128::from(500000u128),
+        }
+    );
+}
+
+#[test]
+fn test_spend() {
+    let mut deps = mock_dependencies(&[]);
+
+    let info = MessageInfo {
+        sender: deps.api.addr_validate("gov").unwrap(),
+        funds: vec![],
+    };
+
+    let msg = InstantiateMsg {
+        gov_contract: info.sender.to_string(),
+        whale_token: "whale".to_string(),
+        spend_limit: Uint128::from(1000000u128),
+    };
+
+    let info = mock_info("addr0000", &[]);
+
+    // we can just call .unwrap() to assert this was a success
+    let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
+
+    // permission failed
+    let msg = ExecuteMsg::Spend {
+        recipient: "addr0000".to_string(),
+        amount: Uint128::from(1000000u128),
+    };
+
+    let info = mock_info("addr0000", &[]);
+
+    match execute(deps.as_mut(), mock_env(), info, msg) {
+        Ok(_) => panic!("Must return error"),
+        Err(ContractError::Unauthorized{ .. }) => (),
+        Err(_) => panic!("Unknown error"),
+    }
+
+    // failed due to spend limit
+    let msg = ExecuteMsg::Spend {
+        recipient: "addr0000".to_string(),
+        amount: Uint128::from(2000000u128),
+    };
+
+    let info = mock_info("gov", &[]);
+    match execute(deps.as_mut(), mock_env(), info, msg) {
+        Ok(_) => panic!("Must return error"),
+        Err(ContractError::TooMuchSpend{ .. }) => (),
+        Err(_) => panic!("Unknown error"),
+    }
+
+    let msg = ExecuteMsg::Spend {
+        recipient: "addr0000".to_string(),
+        amount: Uint128::from(1000000u128),
+    };
+
+    let info = MessageInfo {
+        sender: deps.api.addr_validate("gov").unwrap(),
+        funds: vec![],
+    };
+    let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+    assert_eq!(
+        res.messages,
+        vec![SubMsg::new(CosmosMsg::Wasm(WasmMsg::Execute {
+            contract_addr: "whale".to_string(),
+            funds: vec![],
+            msg: to_binary(&Cw20ExecuteMsg::Transfer {
+                recipient: "addr0000".to_string(),
+                amount: Uint128::from(1000000u128),
+            })
+            .unwrap(),
+        }))]
+    );
 }
