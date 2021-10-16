7910d7066f49c990d4ecea70954b7f8e80869600
CyberHoward <bytephoton@protonmail.com>
2021-10-16 15:20:33 +0000

    Update deploy contracts

--- contracts/community-fund/scripts/bombay/deploy.py
+++ contracts/community-fund/scripts/bombay/deploy.py
@@ -14,29 +14,29 @@ import pathlib
 import sys
 sys.path.append(pathlib.Path(__file__).parent.resolve())
 
-mnemonic = "main jar girl opinion train type cycle blood marble kitchen april champion amount engine crumble tunnel model vicious system student hood fee curious traffic"
+mnemonic = "coin reunion grab unlock jump reason year estate device elevator clean orbit pencil spawn very hope floor actual very clay stereo federal correct beef"
 std_fee = StdFee(5000000, "1500000uusd")
 
 deployer = get_deployer(mnemonic=mnemonic, chain_id="bombay-12", fee=std_fee)
 
-# print("store contract")
-# code_id = deployer.store_contract(contract_name="buy_and_burn")
-# print(f"stored {code_id}")
-# print("instantiate contract")
-# contract_address = deployer.instantiate_contract(code_id=code_id, init_msg={
-#     "whale_token_addr": whale_token,
-#     "whale_pair_addr": whale_ust_pool,
-#     "anchor_money_market_addr": anchor_money_market,
-#     "aust_addr": aust,
-#     "anchor_deposit_threshold": str(int(10)*int(10**6)),
-#     "anchor_withdraw_threshold": str(int(1)*int(10**4)),
-#     "anchor_deposit_ratio": "0.5"
-# })
-# print(f'instantiated {contract_address}')
-
-result = deployer.execute_contract(community_fund, {
-    "update_admin": {
-        "admin": governance
-    }
+print("store contract")
+code_id = deployer.store_contract(contract_name="buy_and_burn")
+print(f"stored {code_id}")
+print("instantiate contract")
+contract_address = deployer.instantiate_contract(code_id=code_id, init_msg={
+    "whale_token_addr": whale_token,
+    "whale_pair_addr": whale_ust_pool,
+    "anchor_money_market_addr": anchor_money_market,
+    "aust_addr": aust,
+    "anchor_deposit_threshold": str(int(10)*int(10**6)),
+    "anchor_withdraw_threshold": str(int(1)*int(10**4)),
+    "anchor_deposit_ratio": "0.5"
 })
-print(result)
+print(f'instantiated {contract_address}')
+
+# result = deployer.execute_contract(community_fund, {
+#     "update_admin": {
+#         "admin": governance
+#     }
+# })
+# print(result)
--- contracts/stablecoin-vault/scripts/bombay/deploy.py
+++ contracts/stablecoin-vault/scripts/bombay/deploy.py
@@ -16,8 +16,8 @@ from white_whale.address.bombay.terraswap import pools
 from white_whale.address.bombay.white_whale import community_fund, war_chest
 
 
-mnemonic = "napkin guess language merit split slice source happy field search because volcano staff section depth clay inherit result assist rubber list tilt chef start"
-# mnemonic = "flavor addict aunt cream vendor grocery prison chicken fee brother desert science shoulder isolate eyebrow zero rough together delay dose cliff world sugar viable"
+# mnemonic = "napkin guess language merit split slice source happy field search because volcano staff section depth clay inherit result assist rubber list tilt chef start"
+mnemonic = "coin reunion grab unlock jump reason year estate device elevator clean orbit pencil spawn very hope floor actual very clay stereo federal correct beef"
 std_fee = StdFee(6900000, "3500000uusd")
 
 deployer = get_deployer(mnemonic=mnemonic, chain_id="bombay-12", fee=std_fee)
@@ -33,9 +33,7 @@ def get_contract_address(result):
 
 def deploy():
     # store profit check contract
-    print("store contract")
     code_id = deployer.store_contract(contract_name="profit_check")
-    print(f"stored {code_id} {type(code_id)}")
 
     print("instantiate contract")
     profit_check_address = deployer.instantiate_contract(code_id=code_id, init_msg={
@@ -43,11 +41,10 @@ def deploy():
         "denom": "uusd"
     })
     print(f'instantiated profit check {profit_check_address}')
-
+    deployer.store_contract_addr("profit_check",profit_check_address)
     # store stablecoin vault contract
     print("store contract")
     code_id = deployer.store_contract(contract_name="stablecoin_vault")
-    print(f"stored {code_id} {type(code_id)}")
     print("instantiate contract")
     contract_address = deployer.instantiate_contract(code_id=code_id, init_msg={
         "pool_address": pools['UST'].contract_address,
@@ -58,33 +55,84 @@ def deploy():
         "anchor_money_market_address": anchor_money_market,
         "seignorage_address": seignorage,
         "profit_check_address": profit_check_address,
-        "slippage": "0.01",
         "token_code_id": 148,
         "community_fund_addr": community_fund,
         "warchest_addr": war_chest,
         "warchest_fee": "0.001",
         "community_fund_fee": "0.005",
         "max_community_fund_fee": "1000000",
-        "denom": "uusd",
-        "anchor_min_withdraw_amount": "100000000"
+        "denom": "uusd", 
+        "stable_cap": "1000000000"
     }, get_contract_address=get_contract_address)
     print(f'instantiated {contract_address}')
-
+    deployer.store_contract_addr("stablecoin_vault",contract_address)
     # configure stablecoin vault as vault in profit_check
     result = deployer.execute_contract(contract_addr=profit_check_address, execute_msg={
         "set_vault": {
             "vault_address": contract_address
         }
     })
-    print(result)
 
-    return contract_address, profit_check_address
+    result = deployer.client.wasm.contract_query(contract_address, {
+    "config": {}
+    })
+    lp_token_address = result["liquidity_token"]
+    deployer.store_contract_addr("liquidity_token",lp_token_address)
+
 
 
 # deploy()
 # exit()
-# profit_check_address = "terra150y2gjkvk7cr26dxjjxrayc4xh4qgvle96s0ts"
-contract_address = "terra1my6hhy0msz4vaq93smcr7ts59manxx5hlurn7h"
+
+sc_addr = deployer.get_address_dict()
+print(sc_addr)
+vault = sc_addr["stablecoin_vault"]
+lp_token_address = sc_addr["liquidity_token"]
+
+result = deployer.client.wasm.contract_query(lp_token_address, {
+    "balance": {
+        "address": deployer.wallet.key.acc_address
+    }
+})
+lp_balance = int(result["balance"])
+print(f'lp {lp_balance}')
+
+# amount = 1100*1000*1000
+amount = 30000*1000*1000
+result = deployer.execute_contract(contract_addr=vault, execute_msg={
+    "provide_liquidity": {
+        "asset": {
+            "info": {
+                "native_token": { "denom": "uusd" }
+            },
+            "amount": str(amount)
+        }
+    }
+}, coins=str(amount) + "uusd")
+print(result)
+
+result = deployer.client.wasm.contract_query(vault, {
+    "pool": {}
+})
+print(result)
+
+result = deployer.client.wasm.contract_query(lp_token_address, {
+    "balance": {
+        "address": deployer.wallet.key.acc_address
+    }
+})
+print(result)
+exit()
+
+# msg = base64.b64encode(bytes(json.dumps({"withdraw_liquidity": {}}), 'ascii')).decode()
+# result = deployer.execute_contract(contract_addr=lp_token_address, execute_msg={
+#     "send": {
+#         "contract": contract_address,
+#         "amount": str(lp_balance),
+#         "msg": msg
+#     }
+# })
+# print(result)
 
 # result = client.wasm.contract_query(contract_address, {
 #     "config": {}
@@ -184,55 +232,7 @@ contract_address = "terra1my6hhy0msz4vaq93smcr7ts59manxx5hlurn7h"
 # result = client.treasury.tax_rate()
 # print(f'tax = {result}')
 
-result = deployer.client.wasm.contract_query(contract_address, {
-    "config": {}
-})
-lp_token_address = result["liquidity_token"]
 
-result = deployer.client.wasm.contract_query(lp_token_address, {
-    "balance": {
-        "address": deployer.wallet.key.acc_address
-    }
-})
-lp_balance = int(result["balance"])
-print(f'lp {lp_balance}')
-
-# amount = 1100*1000*1000
-amount = 1000*1000*1000
-result = deployer.execute_contract(contract_addr=contract_address, execute_msg={
-    "provide_liquidity": {
-        "asset": {
-            "info": {
-                "native_token": { "denom": "uusd" }
-            },
-            "amount": str(amount)
-        }
-    }
-}, coins=str(amount) + "uusd")
-print(result)
-
-# msg = base64.b64encode(bytes(json.dumps({"withdraw_liquidity": {}}), 'ascii')).decode()
-# result = deployer.execute_contract(contract_addr=lp_token_address, execute_msg={
-#     "send": {
-#         "contract": contract_address,
-#         "amount": str(lp_balance),
-#         "msg": msg
-#     }
-# })
-# print(result)
-
-result = deployer.client.wasm.contract_query(contract_address, {
-    "pool": {}
-})
-print(result)
-
-result = deployer.client.wasm.contract_query(lp_token_address, {
-    "balance": {
-        "address": deployer.wallet.key.acc_address
-    }
-})
-print(result)
-exit()
 # contract_address = "terra1fg79czuq76nt699g96q2z9767gufpz8xx4s8k4"
 
 # res = requests.get("https://fcd.terra.dev/v1/txs/gas_prices")
