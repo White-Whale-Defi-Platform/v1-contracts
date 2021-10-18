f27b7addf1c61eb8161f14117747a4879d5cc611
CyberHoward <bytephoton@protonmail.com>
2021-10-18 15:28:30 +0000

    Deploy script now integrated with SDK

--- contracts/stablecoin-vault/scripts/bombay/deploy.py
+++ contracts/stablecoin-vault/scripts/bombay/deploy.py
@@ -10,11 +10,7 @@ sys.path.append(pathlib.Path(__file__).parent.resolve())
 
 from terra_sdk.core.auth import StdFee
 from white_whale.deploy import get_deployer
-from white_whale.address.bombay.anchor import anchor_money_market, aust
-from white_whale.address.bombay.terra import seignorage
-from white_whale.address.bombay.terraswap import pools
-from white_whale.address.bombay.white_whale import community_fund, war_chest
-
+from white_whale.contracts.stable_vault import StableVaultContract
 
 # mnemonic = "napkin guess language merit split slice source happy field search because volcano staff section depth clay inherit result assist rubber list tilt chef start"
 mnemonic = "coin reunion grab unlock jump reason year estate device elevator clean orbit pencil spawn very hope floor actual very clay stereo federal correct beef"
@@ -22,67 +18,20 @@ std_fee = StdFee(6900000, "3500000uusd")
 
 deployer = get_deployer(mnemonic=mnemonic, chain_id="bombay-12", fee=std_fee)
 
-def get_contract_address(result):
-    log = json.loads(result.raw_log)
-    contract_address = ''
-    for entry in log[0]['events'][0]['attributes']:
-        if entry['key'] == 'contract_address':
-            contract_address = entry['value']
-    return contract_address
+vault = StableVaultContract(deployer)
+# vault.create()
 
+lp_balance = vault.query_lp_balance()
+print(f'lp {lp_balance}')
+# vault.withdraw_all()
+while True:
+    # vault.provide_liquidity(2_000_000)
+    lp_balance = vault.query_lp_balance()
+    vault.withdraw_liquidity(lp_balance/2)
+lp_balance = vault.query_lp_balance()
+print(f'lp {lp_balance}')
 
-def deploy():
-    # store profit check contract
-    code_id = deployer.store_contract(contract_name="profit_check")
-
-    print("instantiate contract")
-    profit_check_address = deployer.instantiate_contract(code_id=code_id, init_msg={
-        "vault_address": deployer.wallet.key.acc_address,
-        "denom": "uusd"
-    })
-    print(f'instantiated profit check {profit_check_address}')
-    deployer.store_contract_addr("profit_check",profit_check_address)
-    # store stablecoin vault contract
-    print("store contract")
-    code_id = deployer.store_contract(contract_name="stablecoin_vault")
-    print("instantiate contract")
-    contract_address = deployer.instantiate_contract(code_id=code_id, init_msg={
-        "pool_address": pools['UST'].contract_address,
-        "asset_info": {
-            "native_token": { "denom": "uusd" }
-        },
-        "aust_address": aust,
-        "anchor_money_market_address": anchor_money_market,
-        "seignorage_address": seignorage,
-        "profit_check_address": profit_check_address,
-        "token_code_id": 148,
-        "community_fund_addr": community_fund,
-        "warchest_addr": war_chest,
-        "warchest_fee": "0.001",
-        "community_fund_fee": "0.005",
-        "max_community_fund_fee": "1000000",
-        "denom": "uusd", 
-        "stable_cap": "1000000000"
-    }, get_contract_address=get_contract_address)
-    print(f'instantiated {contract_address}')
-    deployer.store_contract_addr("stablecoin_vault",contract_address)
-    # configure stablecoin vault as vault in profit_check
-    result = deployer.execute_contract(contract_addr=profit_check_address, execute_msg={
-        "set_vault": {
-            "vault_address": contract_address
-        }
-    })
-
-    result = deployer.client.wasm.contract_query(contract_address, {
-    "config": {}
-    })
-    lp_token_address = result["liquidity_token"]
-    deployer.store_contract_addr("liquidity_token",lp_token_address)
-
-
-
-# deploy()
-# exit()
+exit()
 
 sc_addr = deployer.get_address_dict()
 print(sc_addr)
@@ -95,7 +44,6 @@ result = deployer.client.wasm.contract_query(lp_token_address, {
     }
 })
 lp_balance = int(result["balance"])
-print(f'lp {lp_balance}')
 
 # msg = base64.b64encode(bytes(json.dumps({"withdraw_liquidity": {}}), 'ascii')).decode()
 # result = deployer.execute_contract(contract_addr=lp_token_address, execute_msg={
@@ -107,15 +55,15 @@ print(f'lp {lp_balance}')
 # })
 # print(result)
 
-amount = 1100*1000*1000
-# amount = 30000*1000*1000
+# amount = 1100*1000*1000
+amount = 200*1000*1000
 result = deployer.execute_contract(contract_addr=vault, execute_msg={
     "provide_liquidity": {
         "asset": {
             "info": {
-                "native_token": { "denom": "uusd" }
+                "native_token": { "denom": "uusd" },
             },
-            "amount": str(amount)
+            "amount": str(amount),
         }
     }
 }, coins=str(amount) + "uusd")
