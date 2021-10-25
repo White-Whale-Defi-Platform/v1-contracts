de4c95e86996dfa0f6743ba7e8884214cc271ea4
CyberHoward <bytephoton@protonmail.com>
2021-10-25 21:53:40 +0000

    arb strat testing

--- contracts/stablecoin-vault/src/tests/instantiate.rs
+++ contracts/stablecoin-vault/src/tests/instantiate.rs
@@ -1,21 +1,19 @@
-use cosmwasm_std::testing::{mock_env, mock_info, MOCK_CONTRACT_ADDR};
-use cosmwasm_std::{coins, from_binary, to_binary, DepsMut, MessageInfo, ReplyOn, SubMsg, WasmMsg};
+use cosmwasm_std::testing::{mock_env, mock_info};
+use cosmwasm_std::{from_binary, to_binary, DepsMut, MessageInfo, ReplyOn, SubMsg, WasmMsg};
 use cosmwasm_std::{Api, CanonicalAddr, Decimal, Uint128};
 
 use crate::contract::{execute, instantiate, query};
-use crate::state::{State, ADMIN, DEPOSIT_INFO, FEE, POOL_INFO, STATE};
-use cw20::{Cw20ExecuteMsg, Cw20ReceiveMsg, MinterResponse};
-use cw_controllers::Admin;
-use cw_storage_plus::Item;
+use crate::state::{State, STATE};
+use cw20::MinterResponse;
+
 use terraswap::asset::AssetInfo;
 use terraswap::token::InstantiateMsg as TokenInstantiateMsg;
 use white_whale::fee::*;
 use white_whale::ust_vault::msg::VaultQueryMsg as QueryMsg;
 use white_whale::ust_vault::msg::*;
 
-use crate::tests::common::{ARB_CONTRACT, LP_TOKEN, TEST_CREATOR};
+use crate::tests::common::{ARB_CONTRACT, TEST_CREATOR};
 
-use crate::error::StableVaultError;
 use crate::tests::mock_querier::mock_dependencies;
 
 const INSTANTIATE_REPLY_ID: u8 = 1u8;
