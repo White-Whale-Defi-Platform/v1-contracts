d6233bea4dc05fb14d6232c51ed8314e39a6a79b
Rahul Mittal <84127070+alienHunterOnMars@users.noreply.github.com>
2021-11-06 22:39:18 +0530

    updated schemas

--- contracts/vesting/examples/schema.rs
+++ contracts/vesting/examples/schema.rs
@@ -3,9 +3,9 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use white_whale_vesting::msg::{
-    ConfigResponse, ExecuteMsg, InstantiateMsg, QueryMsg, VestingAccountResponse,
-    VestingAccountsResponse,
+use white_whale::vesting::{ InstantiateMsg, ReceiveMsg, 
+    ConfigResponse, ExecuteMsg, QueryMsg, StateResponse,
+    SimulateWithdrawResponse, AllocationInfo, Schedule
 };
 
 fn main() {
@@ -16,8 +16,11 @@ fn main() {
 
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(ReceiveMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(ConfigResponse), &out_dir);
-    export_schema(&schema_for!(VestingAccountResponse), &out_dir);
-    export_schema(&schema_for!(VestingAccountsResponse), &out_dir);
+    export_schema(&schema_for!(StateResponse), &out_dir);
+    export_schema(&schema_for!(SimulateWithdrawResponse), &out_dir);
+    export_schema(&schema_for!(AllocationInfo), &out_dir);
+    export_schema(&schema_for!(Schedule), &out_dir);
 }
