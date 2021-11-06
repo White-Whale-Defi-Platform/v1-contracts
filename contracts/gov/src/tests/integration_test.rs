b744ff9a6ae7d7466e74f952c3cfc6a3cfa5f722
0xFable <0xfable@protonmail.com>
2021-11-06 05:37:39 +0000

    feat(store-code): Store code and instantiate from store in  cw-multi-test

--- contracts/gov/src/tests/integration_test.rs
+++ contracts/gov/src/tests/integration_test.rs
@@ -17,7 +17,7 @@ use stablecoin_vault::contract::{execute, instantiate, query, reply};
 use stablecoin_vault::response::{MsgInstantiateContractResponse};
 use stablecoin_vault::error::StableVaultError;
 use terraswap::asset::{Asset, AssetInfo};
-// use terraswap::pair::{InstantiateMsg, instantiate, query};
+use terraswap::pair::{InstantiateMsg, instantiate, query};
 
 use schemars::JsonSchema;
 use std::fmt::Debug;
@@ -49,5 +49,46 @@ pub fn mock_app() -> App<Empty> {
     App::new(api, env.block, bank, MockStorage::new())
   }
 
+#[test]
+// receive cw20 tokens and release upon approval
+fn goverance_can_update_the_slippage_parameter_of_vault() {
+    // set personal balance
+    let owner = Addr::unchecked("owner");
+    let init_funds = coins(2000, "btc");
+
+    let mut router = mock_app();
+
+    // Store the stablecoin vault as a code object 
+    let vault_id = router.store_code(contract_stablecoin_vault());
+    // Next prepare an instantiate message for that contract
+    let vault_msg = vault_instan_msg();
+    // Store the gov contract as a code object 
+    let gov_id = router.store_code(contract_gov());
+    let gov_msg = gov_instan_msg();
+
+    // First setup the gov 
+    let gov_addr = router
+        .instantiate_contract(gov_id, owner.clone(), &gov_msg, &[], "GOV", None)
+        .unwrap();
+
+    
+    // Next setup the vault 
+    let vault_addr = router
+        .instantiate_contract(vault_id, gov_addr.clone(), &vault_msg, &[], "VAULT", None)
+        .unwrap();
+    // they are different
+    assert_ne!(gov_addr, vault_addr);
+    // TODO: Improve such that a Poll is created with the Gov contract and the Poll contains a message to 
+    // change the slippage param on the vault
+    // This would be the proper way to update it as it is not expected
+    // let slippage_change_msg = stablecoin_vault::msg::ExecuteMsg::SetSlippage {
+    //     slippage: Decimal::percent(5u64)
+    // };
+
+    // let res = router
+    //     .execute_contract(gov_addr.clone(), vault_addr.clone(), &slippage_change_msg, &[])
+    //     .unwrap();
+    // println!("{:?}", res.events);
+}
 
 
