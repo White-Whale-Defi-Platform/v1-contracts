cb76d2f134aa2778bf7e8f5951dabdff87cbd883
CyberHoward <bytephoton@protonmail.com>
2021-11-11 20:38:13 +0000

    update packages

---
+++ packages/white_whale/src/fee.rs
@@ -0,0 +1,89 @@
+use cosmwasm_std::{Addr, CanonicalAddr, Decimal, Uint128};
+use cosmwasm_std::{CosmosMsg, Deps, StdResult};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+use terraswap::asset::Asset;
+
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct Fee {
+    pub share: Decimal,
+}
+
+impl Fee {
+    pub fn compute(&self, amount: Uint128) -> Uint128 {
+        amount * self.share
+    }
+
+    pub fn msg(&self, deps: Deps, asset: Asset, recipient: Addr) -> StdResult<CosmosMsg> {
+        Ok(asset.into_msg(&deps.querier, recipient)?)
+    }
+}
+
+// #[derive(Deserialize, Serialize, Clone, Debug, PartialEq, JsonSchema)]
+// pub struct CappedFee {
+//     pub fee: Fee,
+//     pub max_fee: Uint128,
+// }
+
+// impl CappedFee {
+//     pub fn compute(&self, value: Uint128) -> Uint128 {
+//         min(self.fee.compute(value), self.max_fee)
+//     }
+
+//     pub fn msg<T: Clone + fmt::Debug + PartialEq + JsonSchema>(
+//         &self,
+//         deps: Deps,
+//         value: Uint128,
+//         denom: String,
+//         address: String,
+//     ) -> StdResult<CosmosMsg<T>> {
+//         let fee = self.compute(value);
+//         let community_fund_asset = Asset {
+//             info: AssetInfo::NativeToken { denom },
+//             amount: fee,
+//         };
+
+//         Ok(CosmosMsg::Wasm(WasmMsg::Execute {
+//             contract_addr: address,
+//             funds: vec![community_fund_asset.deduct_tax(&deps.querier)?],
+//             msg: to_binary(&CommunityFundMsg::Deposit {})?,
+//         }))
+//     }
+// }
+
+#[derive(Deserialize, Serialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct VaultFee {
+    pub flash_loan_fee: Fee,
+    pub warchest_fee: Fee,
+    pub warchest_addr: CanonicalAddr,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_fee() {
+        let fee = Fee {
+            share: Decimal::percent(20u64),
+        };
+        let deposit = Uint128::from(1000000u64);
+        let deposit_fee = fee.compute(deposit);
+        assert_eq!(deposit_fee, Uint128::from(200000u64));
+    }
+
+    // #[test]
+    // fn test_capped_fee() {
+    //     let max_fee = Uint128::from(1000u64);
+    //     let fee = CappedFee {
+    //         fee: Fee {
+    //             share: Decimal::percent(20u64),
+    //         },
+    //         max_fee,
+    //     };
+    //     let deposit = Uint128::from(1000000u64);
+    //     let deposit_fee = fee.compute(deposit);
+    //     assert_eq!(deposit_fee, max_fee);
+    // }
+}
---
+++ packages/white_whale/src/ust_vault/msg.rs
@@ -0,0 +1,128 @@
+use crate::fee::{Fee, VaultFee};
+use cosmwasm_std::{
+    to_binary, Addr, Binary, Coin, CosmosMsg, Decimal, StdResult, Uint128, WasmMsg,
+};
+use cw20::Cw20ReceiveMsg;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use std::fmt;
+use terraswap::asset::{Asset, AssetInfo};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub anchor_money_market_address: String,
+    pub aust_address: String,
+    pub profit_check_address: String,
+    pub warchest_addr: String,
+    pub asset_info: AssetInfo,
+    pub token_code_id: u64,
+    pub warchest_fee: Decimal,
+    pub flash_loan_fee: Decimal,
+    pub stable_cap: Uint128,
+    pub vault_lp_token_name: Option<String>,
+    pub vault_lp_token_symbol: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    Receive(Cw20ReceiveMsg),
+    ProvideLiquidity {
+        asset: Asset,
+    },
+    SetStableCap {
+        stable_cap: Uint128,
+    },
+    SetFee {
+        flash_loan_fee: Option<Fee>,
+        warchest_fee: Option<Fee>,
+    },
+    SetAdmin {
+        admin: String,
+    },
+    AddToWhitelist {
+        contract_addr: String,
+    },
+    RemoveFromWhitelist {
+        contract_addr: String,
+    },
+    UpdateState {
+        anchor_money_market_address: Option<String>,
+        aust_address: Option<String>,
+        profit_check_address: Option<String>,
+        allow_non_whitelisted: Option<bool>,
+    },
+    FlashLoan {
+        payload: FlashLoanPayload,
+    },
+    Callback(CallbackMsg),
+}
+
+// Modified from
+// https://github.com/CosmWasm/cosmwasm-plus/blob/v0.2.3/packages/cw20/src/receiver.rs#L15
+impl CallbackMsg {
+    pub fn to_cosmos_msg<T: Clone + fmt::Debug + PartialEq + JsonSchema>(
+        &self,
+        contract_addr: &Addr,
+    ) -> StdResult<CosmosMsg<T>> {
+        Ok(CosmosMsg::Wasm(WasmMsg::Execute {
+            contract_addr: String::from(contract_addr),
+            msg: to_binary(&ExecuteMsg::Callback(self.clone()))?,
+            funds: vec![],
+        }))
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum CallbackMsg {
+    AfterSuccessfulLoanCallback {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct PoolResponse {
+    pub assets: [Asset; 3],
+    pub total_value_in_ust: Uint128,
+    pub total_share: Uint128,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct FlashLoanPayload {
+    pub requested_asset: Asset,
+    pub callback: Binary,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum VaultQueryMsg {
+    Config {},
+    Pool {},
+    Fees {},
+    EstimateWithdrawFee { amount: Uint128 },
+    VaultValue {},
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct DepositResponse {
+    pub deposit: Uint128,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ValueResponse {
+    pub total_ust_value: Uint128,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct FeeResponse {
+    pub fees: VaultFee,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct EstimateDepositFeeResponse {
+    pub fee: Vec<Coin>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct EstimateWithdrawFeeResponse {
+    pub fee: Vec<Coin>,
+}
