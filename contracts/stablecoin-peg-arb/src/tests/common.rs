61b5e3a3fedac5b2842e6c5f408044d7b7d28fe0
CyberHoward <bytephoton@protonmail.com>
2021-11-11 20:27:26 +0000

    update structure

--- contracts/stablecoin-peg-arb/src/tests/common.rs
+++
@@ -1,2 +0,0 @@
-pub(crate) const VAULT_CONTRACT: &str = "vault_contract_address";
-pub(crate) const TEST_CREATOR: &str = "creator";
--- contracts/stablecoin-vault/src/tests/common.rs
+++
@@ -1,2 +0,0 @@
-pub(crate) const ARB_CONTRACT: &str = "arb_contract_address";
-pub(crate) const TEST_CREATOR: &str = "creator";
