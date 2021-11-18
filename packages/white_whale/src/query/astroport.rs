0616a1fba7641fa3e0e818747dbd7bdf34166026
CyberHoward <bytephoton@protonmail.com>
2021-11-18 10:42:40 +0000

    init astro-arb

--- packages/white_whale/src/query/astroport.rs
+++ packages/white_whale/src/query/astroport.rs
@@ -1,7 +1,8 @@
 use cosmwasm_std::{
     to_binary, Addr, Coin, Decimal, Deps, QueryRequest, StdResult, Uint128, WasmQuery,
 };
-use crate::astroport_helper::{Asset, AssetInfo, PoolResponse, QueryMsg, SimulationResponse};
+use terraswap::asset::{Asset, AssetInfo, PairInfo};
+use terraswap::pair::{PoolResponse, QueryMsg, SimulationResponse};
 
 pub fn simulate_swap(deps: Deps, pool_address: Addr, offer_coin: Coin) -> StdResult<Uint128> {
     let response: SimulationResponse =
@@ -30,6 +31,17 @@ pub fn query_pool(deps: Deps, pool_address: Addr) -> StdResult<PoolResponse> {
     }))?;
 
     Ok(response)
+}
+
+// perform a query for the LP Token Pair information using the provided pool_address
+// return only the address. TODO: Review if we should return the full response instead
+pub fn query_lp_token(deps: Deps, pool_address: Addr) -> StdResult<String> {
+    let response: PairInfo = deps.querier.query(&QueryRequest::Wasm(WasmQuery::Smart {
+        contract_addr: pool_address.to_string(),
+        msg: to_binary(&QueryMsg::Pair {})?,
+    }))?;
+
+    Ok(response.liquidity_token)
 }
 
 pub fn pool_ratio(deps: Deps, pool_address: Addr) -> StdResult<Decimal> {
