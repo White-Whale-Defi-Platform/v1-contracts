c93690417e24ed24b638ff47d5142720571c3cd7
Kerber0x <kerber0x@protonmail.com>
2021-11-19 09:40:28 +0100

    Test query

--- contracts/stablecoin-vault/src/tests/query.rs
+++ contracts/stablecoin-vault/src/tests/query.rs
@@ -1,73 +1,66 @@
-use cosmwasm_std::testing::{mock_env, mock_info};
-use cosmwasm_std::{coins, from_binary, to_binary, DepsMut, MessageInfo, ReplyOn, SubMsg, WasmMsg};
+use cosmwasm_std::{coin, coins, DepsMut, from_binary, MessageInfo, ReplyOn, SubMsg, to_binary, WasmMsg};
 use cosmwasm_std::{Api, Decimal, Uint128};
+use cosmwasm_std::testing::{mock_env, mock_info};
+use cw20::MinterResponse;
+use terraswap::asset::{Asset, AssetInfo, AssetInfoRaw};
+use terraswap::token::InstantiateMsg as TokenInstantiateMsg;
+
+use white_whale::fee::*;
+use white_whale::ust_vault::msg::*;
+use white_whale::ust_vault::msg::VaultQueryMsg as QueryMsg;
 
 use crate::contract::{execute, instantiate, query};
-use crate::state::{State, STATE};
-use cw20::MinterResponse;
 use crate::pool_info::{PoolInfo, PoolInfoRaw};
-
-use terraswap::token::InstantiateMsg as TokenInstantiateMsg;
-use white_whale::fee::*;
-use white_whale::ust_vault::msg::VaultQueryMsg as QueryMsg;
-use white_whale::ust_vault::msg::*;
-
-use crate::tests::common::{ARB_CONTRACT, TEST_CREATOR, };
-
-use crate::tests::mock_querier::mock_dependencies;
+use crate::state::{State, STATE};
+use crate::tests::common::{ARB_CONTRACT, TEST_CREATOR};
 use crate::tests::instantiate::mock_instantiate;
+use crate::tests::mock_querier::mock_dependencies;
 
 const INSTANTIATE_REPLY_ID: u8 = 1u8;
-use terraswap::asset::{Asset, AssetInfo, AssetInfoRaw};
+
+/*#[test]
+pub fn test_config_query() {
+    let mut deps = mock_dependencies(&[]);
+    mock_instantiate(deps.as_mut());
+    let env = mock_env();
+
+    let msg = ExecuteMsg::ProvideLiquidity {
+        asset: Asset {
+            info: AssetInfo::NativeToken {
+                denom: String::from("uusd")
+            },
+            amount: Uint128::new(1000),
+        }
+    };
+    let info = mock_info(TEST_CREATOR, &coins(1000, "uusd"));
+    execute(deps.as_mut(), env.clone(), info, msg).unwrap();
 
 
-// #[test]
-// pub fn test_config_query() {
-
-//     let mut deps = mock_dependencies(&[]);
-//     mock_instantiate(deps.as_mut());
-//     let env = mock_env();
-//     let msg = ExecuteMsg::ProvideLiquidity {
-//         asset: Asset {
-//             info: AssetInfo::NativeToken{
-//                 denom: String::from("uusd")
-//             },
-//             amount: Uint128::new(1000)
-//         }
-//         };
-
-
-//     let info = mock_info(TEST_CREATOR, &coins(1000, "uusd"));
-//     let execute_res = execute(deps.as_mut(), env.clone(), info, msg);
-    
-
-//     let q_res: PoolInfo =
-//         from_binary(&query(deps.as_ref(), env, QueryMsg::Config {}).unwrap()).unwrap();
-//     assert_eq!(
-//         q_res.stable_cap,
-//         Uint128::from(100_000_000u64)
-//     )
-// }
+    let q_res: PoolInfo = from_binary(&query(deps.as_ref(), env, QueryMsg::Config {}).unwrap()).unwrap();
+    assert_eq!(
+        q_res.stable_cap,
+        Uint128::from(100_000_000u64)
+    )
+}*/
 
 #[test]
 pub fn test_state_query() {
-
     let mut deps = mock_dependencies(&[]);
     mock_instantiate(deps.as_mut());
     let env = mock_env();
     let msg = ExecuteMsg::ProvideLiquidity {
         asset: Asset {
-            info: AssetInfo::NativeToken{
+            info: AssetInfo::NativeToken {
                 denom: String::from("uusd")
             },
-            amount: Uint128::new(1000)
+            amount: Uint128::new(1000),
         }
-        };
+    };
 
 
     let info = mock_info(TEST_CREATOR, &coins(1000, "uusd"));
     let execute_res = execute(deps.as_mut(), env.clone(), info, msg);
-    
+
 
     let q_res: StateResponse =
         from_binary(&query(deps.as_ref(), env, QueryMsg::State {}).unwrap()).unwrap();
@@ -77,17 +70,13 @@ pub fn test_state_query() {
     )
 }
 
-// #[test]
+//#[test]
 pub fn test_pool_query() {
-
-    let mut deps = mock_dependencies(&[]);
+    let mut deps = mock_dependencies(&coins(1000, "uusd"));
     mock_instantiate(deps.as_mut());
     let env = mock_env();
 
-
-
-    let info = mock_info(TEST_CREATOR, &coins(1000, "uusd"));
-
+    let info = mock_info(TEST_CREATOR, &[]);
 
     let q_res: PoolResponse =
         from_binary(&query(deps.as_ref(), env, QueryMsg::Pool {}).unwrap()).unwrap();
@@ -97,7 +86,7 @@ pub fn test_pool_query() {
             Asset {
                 amount: Uint128::from(10000u128),
                 info: AssetInfo::NativeToken {
-                    denom: "whale".to_string(),
+                    denom: "uwhale".to_string(),
                 },
             },
             Asset {
@@ -116,12 +105,10 @@ pub fn test_pool_query() {
 
 #[test]
 pub fn test_fees_query() {
-
     let mut deps = mock_dependencies(&[]);
     mock_instantiate(deps.as_mut());
     let env = mock_env();
 
-    
 
     let q_res: FeeResponse =
         from_binary(&query(deps.as_ref(), env, QueryMsg::Fees {}).unwrap()).unwrap();
@@ -135,25 +122,11 @@ pub fn test_fees_query() {
     );
 }
 
-// #[test]
+#[test]
 pub fn test_vault_value_query() {
-
-    let mut deps = mock_dependencies(&[]);
+    let mut deps = mock_dependencies(&[coin(1000, "uusd")]);
     mock_instantiate(deps.as_mut());
     let env = mock_env();
-    let msg = ExecuteMsg::ProvideLiquidity {
-        asset: Asset {
-            info: AssetInfo::NativeToken{
-                denom: String::from("uusd")
-            },
-            amount: Uint128::new(1_000_000_000)
-        }
-        };
-
-
-    let info = mock_info(TEST_CREATOR, &coins(1_000_000_000, "uusd"));
-    let execute_res = execute(deps.as_mut(), env.clone(), info, msg);
-
 
     let q_res: ValueResponse =
         from_binary(&query(deps.as_ref(), env, QueryMsg::VaultValue {}).unwrap()).unwrap();
@@ -163,28 +136,14 @@ pub fn test_vault_value_query() {
     )
 }
 
-// #[test]
+//#[test]
 pub fn test_vault_estimate_fee_query() {
-
-    let mut deps = mock_dependencies(&[]);
+    let mut deps = mock_dependencies(&[coin(1000, "uusd")]);
     mock_instantiate(deps.as_mut());
     let env = mock_env();
-    let msg = ExecuteMsg::ProvideLiquidity {
-        asset: Asset {
-            info: AssetInfo::NativeToken{
-                denom: String::from("uusd")
-            },
-            amount: Uint128::new(1000)
-        }
-        };
-
-
-    let info = mock_info(TEST_CREATOR, &coins(1000, "uusd"));
-    let execute_res = execute(deps.as_mut(), env.clone(), info, msg);
-
 
     let q_res: EstimateWithdrawFeeResponse =
-        from_binary(&query(deps.as_ref(), env, QueryMsg::EstimateWithdrawFee {amount: Uint128::new(1000)}).unwrap()).unwrap();
+        from_binary(&query(deps.as_ref(), env, QueryMsg::EstimateWithdrawFee { amount: Uint128::new(1000) }).unwrap()).unwrap();
     assert_ne!(
         q_res.fee,
         vec![]
