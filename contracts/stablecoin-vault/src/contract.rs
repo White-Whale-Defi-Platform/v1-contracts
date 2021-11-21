76c731dad12c7ded2545edfec4f37887c88f3a00
--global <cyberhoward@protonmail.com>
2021-11-21 12:24:24 +0000

    fix

--- contracts/stablecoin-vault/src/contract.rs
+++ contracts/stablecoin-vault/src/contract.rs
@@ -602,10 +602,12 @@ pub fn get_withdraw_fee(deps: Deps, amount: Uint128) -> StdResult<Uint128> {
         deps,
         &Coin::new((amount - warchest_fee).u128(), String::from("uusd")),
     )?;
+    let stable_transfer_fee = compute_tax(
+        deps,
+        &Coin::new((amount - warchest_fee - anchor_withdraw_fee).u128(), String::from("uusd")),
+    )?;
     // Two transfers (anchor -> vault -> user) so ~2x tax. 
-    // Small deviation on small withdrawals is acceptable.
-    // This function is only used for front-end. 
-    Ok(warchest_fee + Uint128::from(2u8) * anchor_withdraw_fee)
+    Ok(warchest_fee + anchor_withdraw_fee + stable_transfer_fee)
 }
 
 //----------------------------------------------------------------------------------------
