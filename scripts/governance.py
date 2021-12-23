610234c9d1a1b3969a53f39766f2cf10347fd407
--global <cyberhoward@protonmail.com>
2021-12-23 11:15:47 +0000

    add script

---
+++ scripts/governance.py
@@ -0,0 +1,40 @@
+import base64
+import json
+
+import pathlib
+import sys
+# temp workaround
+sys.path.append('/workspaces/devcontainer/White-Whale-SDK/src')
+sys.path.append(pathlib.Path(__file__).parent.resolve())
+
+from terra_sdk.core.auth import StdFee
+from white_whale.deploy import get_deployer
+from terra_sdk.core.coins import Coin
+from white_whale.contracts.governance import *
+
+#------------------------
+#   Run with: $ cd /workspaces/devcontainer/contracts ; /usr/bin/env /bin/python3 -- /workspaces/devcontainer/contracts/scripts/ust_vault.py 
+#------------------------
+
+
+# mnemonic = "napkin guess language merit split slice source happy field search because volcano staff section depth clay inherit result assist rubber list tilt chef start"
+mnemonic = "coin reunion grab unlock jump reason year estate device elevator clean orbit pencil spawn very hope floor actual very clay stereo federal correct beef"
+std_fee = StdFee(10*690000, "1200000uusd")
+
+# deployer = get_deployer(mnemonic=mnemonic, chain_id="columbus-5", fee=None)
+deployer = get_deployer(mnemonic=mnemonic, chain_id="bombay-12", fee=None)
+
+gov = Governance(deployer)
+create = False
+
+if create:
+    gov.create()
+    gov.set_token()
+
+deployer.whale_balance()
+gov.stake(1000)
+gov.get_staked_amount()
+# gov.unstake_all()
+
+# gov.create_poll()
+   
