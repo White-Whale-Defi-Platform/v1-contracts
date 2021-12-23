3f77575c5a16bf5c183d7f9db835a0263fbbfeb8
--global <cyberhoward@protonmail.com>
2021-12-23 20:03:20 +0000

    deploy scripts

---
+++ scripts/emissions.py
@@ -0,0 +1,69 @@
+import base64
+import json
+
+import pathlib
+import sys
+import datetime
+# temp workaround
+sys.path.append('/workspaces/devcontainer/White-Whale-SDK/src')
+sys.path.append(pathlib.Path(__file__).parent.resolve())
+
+from terra_sdk.core.auth import StdFee
+from white_whale.deploy import get_deployer
+from terra_sdk.core.coins import Coin
+from white_whale.contracts.emissions import *
+
+#------------------------
+#   Run with: $ cd /workspaces/devcontainer/contracts ; /usr/bin/env /bin/python3 -- /workspaces/devcontainer/contracts/scripts/ust_vault.py 
+#------------------------
+
+#####################
+#   DEPLOY PARAMETERS
+#####################
+
+MILLION = 1_000_000
+
+YEAR = 2021
+MONTH = 12
+DAY = 12
+# 5 AM UTC
+HOUR = 5
+# START_TIME = datetime.datetime(year=YEAR, month=MONTH, day=DAY, hour=HOUR)
+START_TIME = datetime.datetime.utcnow() + datetime.timedelta(minutes=10);
+
+# Start time in linux language
+BLOCK_START_TIME = (START_TIME - datetime.datetime(1970, 1, 1)).total_seconds()
+# 3 day duration (=72h)
+DURATION = datetime.timedelta(days=30)
+# DURATION = datetime.timedelta(minutes=3)
+
+END_TIME = START_TIME + DURATION
+
+NOW = datetime.datetime.utcnow()
+print(f'LBP starts in {START_TIME - NOW} from now.')
+
+print(f'Starts on {START_TIME}')
+print(f'Ends on {END_TIME}')
+
+BLOCK_END_TIME = BLOCK_START_TIME + DURATION.total_seconds()
+
+print(BLOCK_END_TIME - BLOCK_START_TIME)
+print((END_TIME - START_TIME).total_seconds())
+
+# mnemonic = "napkin guess language merit split slice source happy field search because volcano staff section depth clay inherit result assist rubber list tilt chef start"
+mnemonic = "coin reunion grab unlock jump reason year estate device elevator clean orbit pencil spawn very hope floor actual very clay stereo federal correct beef"
+
+# deployer = get_deployer(mnemonic=mnemonic, chain_id="columbus-5", fee=None)
+deployer = get_deployer(mnemonic=mnemonic, chain_id="bombay-12", fee=None)
+
+emissions = Emissions(deployer)
+create = False
+
+if create:
+    # TODO: make multisig owner 
+    emissions.create(int(BLOCK_START_TIME), int(DURATION.total_seconds()))
+
+deployer.whale_balance()
+
+emissions.create_vesting(int(1000), int(BLOCK_START_TIME), int(DURATION.total_seconds()))
+   
--- scripts/governance.py
+++ scripts/governance.py
@@ -16,10 +16,8 @@ from white_whale.contracts.governance import *
 #   Run with: $ cd /workspaces/devcontainer/contracts ; /usr/bin/env /bin/python3 -- /workspaces/devcontainer/contracts/scripts/ust_vault.py 
 #------------------------
 
-
 # mnemonic = "napkin guess language merit split slice source happy field search because volcano staff section depth clay inherit result assist rubber list tilt chef start"
 mnemonic = "coin reunion grab unlock jump reason year estate device elevator clean orbit pencil spawn very hope floor actual very clay stereo federal correct beef"
-std_fee = StdFee(10*690000, "1200000uusd")
 
 # deployer = get_deployer(mnemonic=mnemonic, chain_id="columbus-5", fee=None)
 deployer = get_deployer(mnemonic=mnemonic, chain_id="bombay-12", fee=None)
@@ -32,8 +30,9 @@ if create:
     gov.set_token()
 
 deployer.whale_balance()
-gov.stake(1000)
+# gov.stake(1000)
 gov.get_staked_amount()
+gov.create_poll("")
 # gov.unstake_all()
 
 # gov.create_poll()
