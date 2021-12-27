d9329881a362df354c620179752884d3f12780a2
0xFable <0xfable@protonmail.com>
2021-12-27 17:59:33 +0000

    feat(mock_querier): Rework queier to include TreasuryTax, Astro,Terraswap and some CW20Func

--- contracts/treasury/dapps/astroport/src/tests/mock_querier.rs
+++ contracts/treasury/dapps/astroport/src/tests/mock_querier.rs
@@ -1,14 +1,17 @@
 #![allow(dead_code)]
 use cosmwasm_std::testing::{MockApi, MockQuerier, MockStorage, MOCK_CONTRACT_ADDR};
 use cosmwasm_std::{
-    from_binary, from_slice, to_binary, Coin, ContractResult, Empty, OwnedDeps, Querier,
-    QuerierResult, QueryRequest, SystemError, SystemResult, Uint128, WasmQuery,
+    from_binary, from_slice, to_binary, Binary, Coin, Decimal, ContractResult, Empty, OwnedDeps, Querier,
+    QuerierResult, QueryRequest, SystemError, SystemResult, Uint128, WasmQuery, Response
 };
 use cw20::{BalanceResponse as Cw20BalanceResponse, Cw20QueryMsg};
-use terraswap::asset::Asset;
 use terraswap::pair::{PoolResponse, QueryMsg as TerraswapQueryMsg};
 use white_whale_testing::dapp_base::common::{WHALE_TOKEN, WHALE_UST_PAIR};
 use std::collections::HashMap;
+use cosmwasm_std::Api;
+use terraswap::asset::{Asset, AssetInfo, AssetInfoRaw, PairInfo, PairInfoRaw};
+use cosmwasm_storage::to_length_prefixed;
+use terra_cosmwasm::{TaxCapResponse, TaxRateResponse, TerraQuery,SwapResponse, TerraQuerier, TerraQueryWrapper, TerraRoute};
 
 /// mock_dependencies is a drop-in replacement for cosmwasm_std::testing::mock_dependencies
 /// this uses our CustomQuerier.
@@ -26,8 +29,10 @@ pub fn mock_dependencies(
 }
 
 pub struct WasmMockQuerier {
-    base: MockQuerier<Empty>,
+    base: MockQuerier<TerraQueryWrapper>,
     token_querier: TokenQuerier,
+    astroport_pair_querier: AstroportPairQuerier,
+    tax_querier: TaxQuerier,
 }
 
 #[derive(Clone, Default)]
@@ -59,10 +64,59 @@ pub(crate) fn balances_to_map(
     balances_map
 }
 
+/// The TaxQuerier is used to mock out some routes which are defined as QueryRequest:Custom(_)
+/// the taxquerier combined with TerraMsgQuery allows us to handle requests for both the Terra Treasury and protocols which are compatible with the TerraMsgQuery format (Astro included)
+#[derive(Clone, Default)]
+pub struct TaxQuerier {
+    rate: Decimal,
+    // this lets us iterate over all pairs that match the first string
+    caps: HashMap<String, Uint128>,
+}
+
+impl TaxQuerier {
+    pub fn new(rate: Decimal, caps: &[(&String, &Uint128)]) -> Self {
+        TaxQuerier {
+            rate,
+            caps: caps_to_map(caps),
+        }
+    }
+}
+
+pub(crate) fn caps_to_map(caps: &[(&String, &Uint128)]) -> HashMap<String, Uint128> {
+    let mut owner_map: HashMap<String, Uint128> = HashMap::new();
+    for (denom, cap) in caps.iter() {
+        owner_map.insert(denom.to_string(), **cap);
+    }
+    owner_map
+}
+/// The Astroport PairQuerier is used to handle select operations for Astroport 
+/// and provides a means to define custom response behaviour for common operations
+/// An example included is the pairs. To provide mocked 'pairs' info the func pairs_to_map
+#[derive(Clone, Default)]
+pub struct AstroportPairQuerier {
+    pairs: HashMap<String, PairInfo>,
+}
+
+impl AstroportPairQuerier {
+    pub fn new(pairs: &[(&String, &PairInfo)]) -> Self {
+        AstroportPairQuerier {
+            pairs: pairs_to_map(pairs),
+        }
+    }
+}
+
+pub(crate) fn pairs_to_map(pairs: &[(&String, &PairInfo)]) -> HashMap<String, PairInfo> {
+    let mut pairs_map: HashMap<String, PairInfo> = HashMap::new();
+    for (key, pair) in pairs.iter() {
+        pairs_map.insert(key.to_string(), (*pair).clone());
+    }
+    pairs_map
+}
+
 impl Querier for WasmMockQuerier {
     fn raw_query(&self, bin_request: &[u8]) -> QuerierResult {
         // MockQuerier doesn't support Custom, so we ignore it completely here
-        let request: QueryRequest<Empty> = match from_slice(bin_request) {
+        let request: QueryRequest<TerraQueryWrapper> = match from_slice(bin_request) {
             Ok(v) => v,
             Err(e) => {
                 return SystemResult::Err(SystemError::InvalidRequest {
@@ -76,8 +130,49 @@ impl Querier for WasmMockQuerier {
 }
 
 impl WasmMockQuerier {
-    pub fn handle_query(&self, request: &QueryRequest<Empty>) -> QuerierResult {
+    pub fn handle_query(&self, request: &QueryRequest<TerraQueryWrapper>) -> QuerierResult {
         match &request {
+            QueryRequest::Custom(TerraQueryWrapper { route, query_data }) => {
+                if route == &TerraRoute::Treasury {
+                    match query_data {
+                        TerraQuery::TaxRate {} => {
+                            let res = TaxRateResponse {
+                                rate: self.tax_querier.rate,
+                            };
+                            SystemResult::Ok(ContractResult::from(to_binary(&res)))
+                        }
+                        TerraQuery::TaxCap { denom } => {
+                            let cap = self
+                                .tax_querier
+                                .caps
+                                .get(denom)
+                                .copied()
+                                .unwrap_or_default();
+                            let res = TaxCapResponse { cap };
+                            SystemResult::Ok(ContractResult::from(to_binary(&res)))
+                        }
+                        _ => panic!("DO NOT ENTER HERE"),
+                    }
+                }
+                else if route == &TerraRoute::Market {
+                    match query_data {
+                        TerraQuery::Swap{offer_coin, ask_denom} => {
+                            let res = SwapResponse{
+                                receive: Coin{
+                                    amount: offer_coin.amount,
+                                    denom: String::from(ask_denom)
+                                }
+                            };
+
+                            SystemResult::Ok(ContractResult::from(to_binary(&res)))
+                        }
+                        _ => panic!("DO NOT ENTER HERE"),
+                    }
+                }
+                else {
+                    panic!("DO NOT ENTER HERE")
+                }
+            }
             QueryRequest::Wasm(WasmQuery::Smart { contract_addr, msg }) => {
                 if contract_addr == WHALE_UST_PAIR || contract_addr == "asset_address" {
                     match from_binary(&msg).unwrap() {
@@ -103,12 +198,21 @@ impl WasmMockQuerier {
                         },
                         _ => panic!("DO NOT ENTER HERE"),
                 }} else {
+                    
                     match from_binary(&msg).unwrap() {
+                    
+
                     Cw20QueryMsg::Balance { address } => {
+                        if contract_addr == WHALE_TOKEN {
+                            return SystemResult::Ok(ContractResult::Ok(
+                                to_binary(&Cw20BalanceResponse { balance: Uint128::new(10000) }).unwrap(),
+                            ));
+                        };
                         let balances: &HashMap<String, Uint128> =
                             match self.token_querier.balances.get(contract_addr) {
                                 Some(balances) => balances,
                                 None => {
+                                    
                                     return SystemResult::Err(SystemError::InvalidRequest {
                                         error: format!(
                                             "No balance info exists for the contract {}",
@@ -137,6 +241,7 @@ impl WasmMockQuerier {
                     }
                     _ => panic!("DO NOT ENTER HERE"),
                 }
+                
 
                 }
 
@@ -147,15 +252,21 @@ impl WasmMockQuerier {
 }
 
 impl WasmMockQuerier {
-    pub fn new(base: MockQuerier<Empty>) -> Self {
+    pub fn new(base: MockQuerier<TerraQueryWrapper>) -> Self {
         WasmMockQuerier {
             base,
             token_querier: TokenQuerier::default(),
+            astroport_pair_querier: AstroportPairQuerier::default(),
+            tax_querier: TaxQuerier::default(),
         }
     }
 
     // configure the mint whitelist mock querier
     pub fn with_token_balances(&mut self, balances: &[(&String, &[(&String, &Uint128)])]) {
         self.token_querier = TokenQuerier::new(balances);
+    }
+    // configure the astroport pair
+    pub fn with_astro_pairs(&mut self, pairs: &[(&String, &PairInfo)]) {
+        self.astroport_pair_querier = AstroportPairQuerier::new(pairs);
     }
 }
