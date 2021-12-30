b656f049af19cc67d488a37d8bb54ee1567735e3
--global <cyberhoward@protonmail.com>
2021-12-30 14:50:56 +0000

    Remove Addressbook from base_dapp

--- packages/white_whale/src/treasury/dapp_base/queries.rs
+++ packages/white_whale/src/treasury/dapp_base/queries.rs
@@ -1,25 +1,21 @@
 use cosmwasm_std::{Binary, Deps, StdResult, to_binary};
 
 use crate::treasury::dapp_base::msg::{BaseQueryMsg, BaseStateResponse};
-use crate::treasury::dapp_base::state::{ADDRESS_BOOK, BASESTATE};
+use crate::treasury::dapp_base::state::BASESTATE;
 
 /// Handles the common base queries
 pub fn handle_base_query(deps: Deps, query: BaseQueryMsg) -> StdResult<Binary> {
     match query {
         BaseQueryMsg::Config {} => to_binary(&try_query_config(deps)?),
-        BaseQueryMsg::AddressBook { id } => to_binary(&try_query_addressbook(deps, id)?),
     }
 }
-
+/// Returns the BaseState
 pub fn try_query_config(deps: Deps) -> StdResult<BaseStateResponse> {
     let state = BASESTATE.load(deps.storage)?;
 
     Ok(BaseStateResponse {
         treasury_address: state.treasury_address.into_string(),
         trader: state.trader.into_string(),
+        memory_address: state.memory.address.into_string(),
     })
-}
-
-pub fn try_query_addressbook(deps: Deps, id: String) -> StdResult<String> {
-    ADDRESS_BOOK.load(deps.storage, id.as_str())
 }
--- packages/white_whale/src/treasury/dapp_base/state.rs
+++ packages/white_whale/src/treasury/dapp_base/state.rs
@@ -1,12 +1,11 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
-use cosmwasm_std::{Addr, Deps, StdResult};
+use cosmwasm_std::Addr;
 use cw_controllers::Admin;
-use cw_storage_plus::{Item, Map};
-use terraswap::asset::AssetInfo;
+use cw_storage_plus::Item;
 
-use crate::{denom::is_denom, memory::item::Memory};
+use crate::memory::item::Memory;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 /// The BaseState contains the main addresses needed for sending and verifying messages
@@ -16,35 +15,6 @@ pub struct BaseState {
     pub memory: Memory,
 }
 
-pub const BASESTATE: Item<BaseState> = Item::new("\u{0}{10}base_state");
 // Every DApp should use the provide memory contract for token/contract address resolution
+pub const BASESTATE: Item<BaseState> = Item::new("\u{0}{10}base_state");
 pub const ADMIN: Admin = Admin::new("admin");
-
-// TODO: remove
-// stores name and address of tokens and pairs
-// Example: pairs can be named after the LP token id.
-// LP token key: "ust_luna"
-// Pair key: "ust_luna_pair"
-pub const ADDRESS_BOOK: Map<&str, String> = Map::new("address_book");
-
-// Loads token address from address book. Throws error if its a native token
-pub fn load_contract_addr(deps: Deps, id: &str) -> StdResult<Addr> {
-    deps.api
-        .addr_validate(ADDRESS_BOOK.load(deps.storage, id)?.as_str())
-}
-
-
-// Returns the asset info for an address book entry.
-pub fn get_asset_info(deps: Deps, id: &str) -> StdResult<AssetInfo> {
-    let address_or_denom = ADDRESS_BOOK.load(deps.storage, id)?;
-    return if is_denom(address_or_denom.as_str()) {
-        Ok(AssetInfo::NativeToken {
-            denom: address_or_denom,
-        })
-    } else {
-        deps.api.addr_validate(address_or_denom.as_str())?;
-        Ok(AssetInfo::Token {
-            contract_addr: address_or_denom,
-        })
-    };
-}
