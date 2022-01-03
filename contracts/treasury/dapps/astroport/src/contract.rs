498931e93858205d53a5386142f7637dcdf1ed02
0xFable <0xfable@protonmail.com>
2022-01-03 20:05:20 +0000

    feat(handle_base_init): Update to use helper

--- contracts/treasury/dapps/astroport/src/contract.rs
+++ contracts/treasury/dapps/astroport/src/contract.rs
@@ -3,7 +3,7 @@
 use cosmwasm_std::{entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 
 use white_whale::memory::item::Memory;
-use white_whale::treasury::dapp_base::commands as dapp_base_commands;
+use white_whale::treasury::dapp_base::commands::{self as dapp_base_commands, handle_base_init};
 use white_whale::treasury::dapp_base::common::BaseDAppResult;
 use white_whale::treasury::dapp_base::msg::BaseInstantiateMsg;
 use white_whale::treasury::dapp_base::queries as dapp_base_queries;
@@ -22,14 +22,9 @@ pub fn instantiate(
     info: MessageInfo,
     msg: BaseInstantiateMsg,
 ) -> BaseDAppResult {
-    let state = BaseState {
-        treasury_address: deps.api.addr_validate(&msg.treasury_address)?,
-        trader: deps.api.addr_validate(&msg.trader)?,
-        memory: Memory{ address: deps.api.addr_validate(&msg.memory_addr)?}
-    };
+    let base_state = handle_base_init(deps.as_ref(), msg)?;
 
-    // Store the initial config
-    BASESTATE.save(deps.storage, &state)?;
+    BASESTATE.save(deps.storage, &base_state)?;
 
     // Setup the admin as the creator of the contract
     ADMIN.set(deps, Some(info.sender))?;
