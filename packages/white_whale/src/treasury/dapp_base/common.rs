f88f47807be2e42f684e1446d52eb480ec648181
0xFable <0xfable@protonmail.com>
2022-01-04 00:46:36 +0000

    feat(review): Update anchor dapp after review

--- packages/white_whale/src/treasury/dapp_base/common.rs
+++ packages/white_whale/src/treasury/dapp_base/common.rs
@@ -3,5 +3,5 @@ use crate::treasury::dapp_base::error::BaseDAppError;
 
 /// Postfix for LP pair addresses. 
 pub const PAIR_POSTFIX: &str = "_pair";
-
+pub const ANCHOR_MONEY_MARKET_ID: &str = "anchor_money_market"
 pub type BaseDAppResult = Result<Response, BaseDAppError>;
