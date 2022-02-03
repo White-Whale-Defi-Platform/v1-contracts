bc5d3feb113a11af53f1882c629ebfa376085d9a
0xFable <0xfable@protonmail.com>
2022-02-03 13:19:51 +0000

    fix(fees): Add fee check logic to sc vault and update logic in dapp

--- contracts/stablecoin-vault/src/contract.rs
+++ contracts/stablecoin-vault/src/contract.rs
@@ -838,12 +838,21 @@ pub fn set_fee(
     let mut fee_config = FEE.load(deps.storage)?;
 
     if let Some(fee) = flash_loan_fee {
+        if fee.share >= Decimal::percent(100) {
+            return Err(StableVaultError::FeesTooHigh{});
+        }
         fee_config.flash_loan_fee = fee;
     }
     if let Some(fee) = treasury_fee {
+        if fee.share >= Decimal::percent(100) {
+            return Err(StableVaultError::FeesTooHigh{});
+        }
         fee_config.treasury_fee = fee;
     }
     if let Some(fee) = commission_fee {
+        if fee.share >= Decimal::percent(100) {
+            return Err(StableVaultError::FeesTooHigh{});
+        }
         fee_config.commission_fee = fee;
     }
 
