8412216ef64fb4154996f78c44c6194db83efc1e
Kerber0x <kerber0x@protonmail.com>
2022-04-26 11:40:19 +0100

    Add burn/withdraw messages to anchor dapp

--- packages/white_whale/src/anchor.rs
+++ packages/white_whale/src/anchor.rs
@@ -1,26 +1,30 @@
-use crate::tax::deduct_tax;
+use std::fmt;
+
 use cosmwasm_std::{
-    to_binary, Addr, Coin, CosmosMsg, Deps, ReplyOn, Response, StdError, StdResult, SubMsg,
+    Addr, Coin, CosmosMsg, Deps, ReplyOn, Response, StdError, StdResult, SubMsg, to_binary,
     Uint128, WasmMsg,
 };
 use cw20::Cw20ExecuteMsg;
 use schemars::JsonSchema;
-use std::fmt;
-
 use serde::{Deserialize, Serialize};
+
+use crate::denom::UST_DENOM;
+use crate::tax::deduct_tax;
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum AnchorMsg {
     DepositStable {},
     RedeemStable {},
+    Unbond {},
+    WithdrawUnbonded {},
 }
 
 pub fn try_deposit_to_anchor<T: Clone + fmt::Debug + PartialEq + JsonSchema>(
     anchor_money_market_address: String,
     amount: Coin,
 ) -> StdResult<Response<T>> {
-    if amount.denom != "uusd" {
+    if amount.denom != UST_DENOM {
         return Err(StdError::generic_err(
             "Wrong currency. Only UST (denom: uusd) is supported.",
         ));
@@ -40,7 +44,7 @@ pub fn try_deposit_to_anchor_as_submsg<T: Clone + fmt::Debug + PartialEq + JsonS
     amount: Coin,
     id: u64,
 ) -> StdResult<Response<T>> {
-    if amount.denom != "uusd" {
+    if amount.denom != UST_DENOM {
         return Err(StdError::generic_err(
             "Wrong currency. Only UST (denom: uusd) is supported.",
         ));
@@ -65,7 +69,7 @@ pub fn anchor_deposit_msg<T: Clone + fmt::Debug + PartialEq + JsonSchema>(
     anchor_money_market_address: Addr,
     amount: Coin,
 ) -> StdResult<CosmosMsg<T>> {
-    if amount.denom != "uusd" {
+    if amount.denom != UST_DENOM {
         return Err(StdError::generic_err(
             "Wrong currency. Only UST (denom: uusd) is supported.",
         ));
@@ -90,6 +94,32 @@ pub fn anchor_withdraw_msg<T: Clone + fmt::Debug + PartialEq + JsonSchema>(
             amount,
             msg: to_binary(&AnchorMsg::RedeemStable {})?,
         })?,
+        funds: vec![],
+    }))
+}
+
+pub fn anchor_bluna_unbond_msg<T: Clone + fmt::Debug + PartialEq + JsonSchema>(
+    bluna_address: Addr,
+    bluna_hub_address: Addr,
+    amount: Uint128,
+) -> StdResult<CosmosMsg<T>> {
+    Ok(CosmosMsg::Wasm(WasmMsg::Execute {
+        contract_addr: bluna_address.to_string(),
+        msg: to_binary(&Cw20ExecuteMsg::Send {
+            contract: bluna_hub_address.to_string(),
+            amount,
+            msg: to_binary(&AnchorMsg::Unbond {})?,
+        })?,
+        funds: vec![],
+    }))
+}
+
+pub fn anchor_withdraw_unbonded_msg<T: Clone + fmt::Debug + PartialEq + JsonSchema>(
+    bluna_hub_address: Addr,
+) -> StdResult<CosmosMsg<T>> {
+    Ok(CosmosMsg::Wasm(WasmMsg::Execute {
+        contract_addr: bluna_hub_address.to_string(),
+        msg: to_binary(&AnchorMsg::WithdrawUnbonded {})?,
         funds: vec![],
     }))
 }
