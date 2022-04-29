ae2567ec76b228017d6e4ea9d8d9c35ce2ce1e48
kaimen-sano <kaimen_sano@protonmail.com>
2022-04-29 21:29:20 +1200

    Fix `withdraw_unbonded_bluna`

--- contracts/luna-unbond-handler/src/commands.rs
+++ contracts/luna-unbond-handler/src/commands.rs
@@ -1,18 +1,23 @@
-use cosmwasm_std::{Addr, BankMsg, CosmosMsg, DepsMut, Env, from_binary, MessageInfo, Response, StdError, SubMsg, Uint128};
+use cosmwasm_std::{
+    from_binary, Addr, BankMsg, CosmosMsg, DepsMut, Env, MessageInfo, Response, StdError, SubMsg,
+    Uint128,
+};
 use cw20::Cw20ReceiveMsg;
 use terraswap::asset::{Asset, AssetInfo};
 use terraswap::querier::query_balance;
 
 use white_whale::anchor::{anchor_bluna_unbond_msg, anchor_withdraw_unbonded_msg};
 use white_whale::denom::LUNA_DENOM;
-use white_whale::memory::{ANCHOR_BLUNA_HUB_ID, BLUNA_TOKEN_MEMORY_ID};
 use white_whale::memory::error::MemoryError;
-use white_whale::memory::queries::{query_asset_from_mem, query_contract_from_mem, query_contracts_from_mem};
+use white_whale::memory::queries::{
+    query_asset_from_mem, query_contract_from_mem, query_contracts_from_mem,
+};
+use white_whale::memory::{ANCHOR_BLUNA_HUB_ID, BLUNA_TOKEN_MEMORY_ID};
 use white_whale::ust_vault::msg::ExecuteMsg::Callback;
 
-use crate::{UnbondHandlerError, UnbondHandlerResult};
 use crate::msg::{CallbackMsg, Cw20HookMsg};
 use crate::state::{ADMIN, STATE};
+use crate::{UnbondHandlerError, UnbondHandlerResult};
 
 /// handler function invoked when the unbond handler contract receives
 /// a transaction. This is triggered when someone wants to unbond and withdraw luna from the vault
@@ -27,9 +32,12 @@ pub fn receive_cw20(
             let state = STATE.load(deps.storage)?;
 
             // only bluna token contract can execute this message
-            let bluna_asset_info = query_asset_from_mem(deps.as_ref(), &state.memory_contract, BLUNA_TOKEN_MEMORY_ID)?;
+            let bluna_asset_info =
+                query_asset_from_mem(deps.as_ref(), &state.memory_contract, BLUNA_TOKEN_MEMORY_ID)?;
             match bluna_asset_info {
-                AssetInfo::NativeToken { .. } => return Err(UnbondHandlerError::UnsupportedToken {}),
+                AssetInfo::NativeToken { .. } => {
+                    return Err(UnbondHandlerError::UnsupportedToken {})
+                }
                 AssetInfo::Token { contract_addr } => {
                     if deps.api.addr_validate(&msg_info.sender.to_string())? != contract_addr {
                         return Err(UnbondHandlerError::UnsupportedToken {});
@@ -42,21 +50,33 @@ pub fn receive_cw20(
     }
 }
 
-fn unbond_bluna(
-    deps: DepsMut,
-    _env: Env,
-    amount: Uint128,
-) -> UnbondHandlerResult {
+fn unbond_bluna(deps: DepsMut, _env: Env, amount: Uint128) -> UnbondHandlerResult {
     let state = STATE.load(deps.storage)?;
-    let contracts = [BLUNA_TOKEN_MEMORY_ID.to_string(), ANCHOR_BLUNA_HUB_ID.to_string(), ];
-    let contract_addresses = query_contracts_from_mem(deps.as_ref(), &state.memory_contract, &contracts)?;
+    let contracts = [
+        BLUNA_TOKEN_MEMORY_ID.to_string(),
+        ANCHOR_BLUNA_HUB_ID.to_string(),
+    ];
+    let contract_addresses =
+        query_contracts_from_mem(deps.as_ref(), &state.memory_contract, &contracts)?;
 
     if contract_addresses.len() != 2 {
-        return Err(UnbondHandlerError::Std(StdError::generic_err("couldn't find contracts in memory")));
+        return Err(UnbondHandlerError::Std(StdError::generic_err(
+            "couldn't find contracts in memory",
+        )));
     }
 
-    let bluna_address = contract_addresses.get(BLUNA_TOKEN_MEMORY_ID).ok_or(UnbondHandlerError::MemoryError(MemoryError::NotFoundInMemory {}))?.clone();
-    let bluna_hub_address = contract_addresses.get(ANCHOR_BLUNA_HUB_ID).ok_or(UnbondHandlerError::MemoryError(MemoryError::NotFoundInMemory {}))?.clone();
+    let bluna_address = contract_addresses
+        .get(BLUNA_TOKEN_MEMORY_ID)
+        .ok_or(UnbondHandlerError::MemoryError(
+            MemoryError::NotFoundInMemory {},
+        ))?
+        .clone();
+    let bluna_hub_address = contract_addresses
+        .get(ANCHOR_BLUNA_HUB_ID)
+        .ok_or(UnbondHandlerError::MemoryError(
+            MemoryError::NotFoundInMemory {},
+        ))?
+        .clone();
 
     let bluna_unbond_msg = anchor_bluna_unbond_msg(bluna_address, bluna_hub_address, amount);
     Ok(Response::new()
@@ -67,22 +87,18 @@ fn unbond_bluna(
         .add_message(bluna_unbond_msg))
 }
 
-pub(crate) fn withdraw_unbonded_bluna(
-    deps: DepsMut,
-) -> UnbondHandlerResult {
+pub(crate) fn withdraw_unbonded_bluna(deps: DepsMut, env: Env) -> UnbondHandlerResult {
     let state = STATE.load(deps.storage)?;
-    let bluna_hub_address = query_contract_from_mem(deps.as_ref(), &state.memory_contract, ANCHOR_BLUNA_HUB_ID)?;
+    let bluna_hub_address =
+        query_contract_from_mem(deps.as_ref(), &state.memory_contract, ANCHOR_BLUNA_HUB_ID)?;
 
-    let withdraw_unbonded_msg = SubMsg::new(anchor_withdraw_unbonded_msg(bluna_hub_address));
+    let withdraw_unbonded_msg = SubMsg::new(anchor_withdraw_unbonded_msg(bluna_hub_address)?);
 
     // Callback for after withdrawing the unbonded bluna
     let after_withdraw_msg = CallbackMsg::AfterWithdraw {}.to_cosmos_msg(&env.contract.address)?;
 
     Ok(Response::new()
-        .add_attributes(vec![
-            ("action", "withdraw_unbonded_bluna"),
-            ("amount", amount.to_string()),
-        ])
+        .add_attributes(vec![("action", "withdraw_unbonded_bluna")])
         .add_submessage(withdraw_unbonded_msg)
         .add_message(after_withdraw_msg))
 }
@@ -98,7 +114,13 @@ pub fn set_admin(deps: DepsMut, info: MessageInfo, admin: String) -> UnbondHandl
 }
 
 /// Updates the state of the contract
-pub fn update_state(deps: DepsMut, msg_info: MessageInfo, owner: Option<String>, expiration_time: Option<u64>, memory_contract: Option<String>) -> UnbondHandlerResult {
+pub fn update_state(
+    deps: DepsMut,
+    msg_info: MessageInfo,
+    owner: Option<String>,
+    expiration_time: Option<u64>,
+    memory_contract: Option<String>,
+) -> UnbondHandlerResult {
     // Only the admin should be able to call this
     ADMIN.assert_admin(deps.as_ref(), &msg_info.sender)?;
 
@@ -125,7 +147,12 @@ pub fn update_state(deps: DepsMut, msg_info: MessageInfo, owner: Option<String>,
     Ok(Response::new().add_attributes(attrs))
 }
 
-pub(crate) fn _handle_callback(deps: DepsMut, env: Env, info: MessageInfo, msg: CallbackMsg) -> UnbondHandlerResult {
+pub(crate) fn _handle_callback(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: CallbackMsg,
+) -> UnbondHandlerResult {
     // Callback functions can only be called this contract itself
     if info.sender != env.contract.address {
         return Err(UnbondHandlerError::NotCallback {});
@@ -143,7 +170,9 @@ fn _after_withdraw(deps: DepsMut, env: Env) -> UnbondHandlerResult {
     let refund_amount = query_balance(&deps.querier, env.contract.address, LUNA_DENOM.to_string())?;
     // Construct refund message
     let refund_asset = Asset {
-        info: AssetInfo::NativeToken { denom: LUNA_DENOM.to_string() },
+        info: AssetInfo::NativeToken {
+            denom: LUNA_DENOM.to_string(),
+        },
         amount: refund_amount,
     };
     let taxed_asset = refund_asset.deduct_tax(&deps.querier)?;
--- contracts/luna-unbond-handler/src/contract.rs
+++ contracts/luna-unbond-handler/src/contract.rs
@@ -1,11 +1,11 @@
-use cosmwasm_std::{Addr, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, to_binary};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
+use cosmwasm_std::{to_binary, Addr, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 use cw2::set_contract_version;
 
-use crate::{commands, queries, UnbondHandlerResult};
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state::{State, STATE};
+use crate::{commands, queries, UnbondHandlerResult};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:luna-unbond-handler";
@@ -36,22 +36,23 @@ pub fn instantiate(
     Ok(Response::new()
         .add_attribute("method", "instantiate")
         .add_attribute("owner", state.owner.unwrap_or(Addr::unchecked("")))
-        .add_attribute("expiration_time", state.expiration_time.unwrap_or(0).to_string())
-    )
+        .add_attribute(
+            "expiration_time",
+            state.expiration_time.unwrap_or(0).to_string(),
+        ))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn execute(
-    deps: DepsMut,
-    env: Env,
-    info: MessageInfo,
-    msg: ExecuteMsg,
-) -> UnbondHandlerResult {
+pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> UnbondHandlerResult {
     match msg {
         ExecuteMsg::Receive(msg) => commands::receive_cw20(deps, env, info, msg),
-        ExecuteMsg::WithdrawUnbonded {} => commands::withdraw_unbonded_bluna(deps),
+        ExecuteMsg::WithdrawUnbonded {} => commands::withdraw_unbonded_bluna(deps, env),
         ExecuteMsg::SetAdmin { admin } => commands::set_admin(deps, info, admin),
-        ExecuteMsg::UpdateState { owner, expiration_time, memory_contract } => commands::update_state(deps, info, owner, expiration_time, memory_contract),
+        ExecuteMsg::UpdateState {
+            owner,
+            expiration_time,
+            memory_contract,
+        } => commands::update_state(deps, info, owner, expiration_time, memory_contract),
         ExecuteMsg::Callback(msg) => commands::_handle_callback(deps, env, info, msg),
     }
 }
@@ -59,6 +60,6 @@ pub fn execute(
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::State { } => to_binary(&queries::query_state(deps)?),
+        QueryMsg::State {} => to_binary(&queries::query_state(deps)?),
     }
 }
