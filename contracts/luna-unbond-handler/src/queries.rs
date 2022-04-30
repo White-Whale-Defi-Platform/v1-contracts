dbd5ca8c2b2a42fdd0af654016450b3aaba13ab6
kaimen-sano <kaimen_sano@protonmail.com>
2022-04-30 17:29:28 +1200

    Add Luna Unbond Handler GH workflow

--- contracts/luna-unbond-handler/src/queries.rs
+++ contracts/luna-unbond-handler/src/queries.rs
@@ -2,5 +2,5 @@ use crate::state::{State, STATE};
 use cosmwasm_std::{Deps, StdResult};
 
 pub(crate) fn query_state(deps: Deps) -> StdResult<State> {
-    Ok(STATE.load(deps.storage)?)
+    STATE.load(deps.storage)
 }
